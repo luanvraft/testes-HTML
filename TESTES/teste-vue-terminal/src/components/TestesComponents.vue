fd9df423ba5abb16719c27d3104f8c22181c718a
luan <luanbarroso1212@gmail.com>
2023-07-05 07:58:03 -0300

    update

---
+++ TESTES/teste-vue-terminal/src/components/TestesComponents.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="diferente"> {{ mensagem }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TesteComponent',
+    data() {
+        return {
+            mensagem: 'minha mensagem via meu component'
+        }
+    }
+}
+</script>
+<style>
+h1.diferente {
+    background: black;
+    color: #fff;
+}
+</style>
